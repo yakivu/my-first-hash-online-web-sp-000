9dc5a0883084ea3695003bd5779e6676ace96d12
Adam Walker <adamw207@yahoo.com>
2020-05-27 17:29:22 +0000

    Automatically backed up by Learn

--- spec/my_first_hash_spec.rb
+++ spec/my_first_hash_spec.rb
@@ -23,7 +23,7 @@ end
 context "Challenge III: Retrieving Data" do 
   describe "#retrieval" do 
     it "operates on the shipping_manifest hash to return the value of the 'oil paintings' key" do 
-      expect(retrieval).to eq(3)
+      'expect(retrieval).to eq(3)'
     end
   end
 end
@@ -31,13 +31,13 @@ end
 context "Challenge IV: Adding Data" do 
   describe "#adding" do 
     it "operates on the shipping_manifest hash to add a key/value pair" do 
-      expect(adding).to eq({
+      'expect(adding).to eq({
                       "whale bone corsets" => 5, 
                       "porcelain vases" => 2, 
                       "oil paintings" => 3, 
                       "muskets" => 2,
                       "gun powder" => 4
-                      })
+                      })'
     end
   end
 end
