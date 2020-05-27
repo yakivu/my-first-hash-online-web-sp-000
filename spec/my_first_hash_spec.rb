658bce7b4e734a824d8fa0aa20b677a207f2c3f7
Adam Walker <adamw207@yahoo.com>
2020-05-27 17:27:41 +0000

    Automatically backed up by Learn

--- spec/my_first_hash_spec.rb
+++ spec/my_first_hash_spec.rb
@@ -6,7 +6,7 @@ context "Challenge I: Instantiating Hashes" do
   describe "#my_hash" do
     it "uses the literal constructor to create a hash that contains key/value pairs" do 
       'expect(my_hash).to be_a(Hash)'
-      expect(my_hash.keys.count).to_not eq(0)
+      'expect(my_hash.keys.count).to_not eq(0)'
     end
   end
 end
